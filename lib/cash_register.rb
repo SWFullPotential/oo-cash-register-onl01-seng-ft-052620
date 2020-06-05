e9e4b04d0aa56725b825543f9dd0fce85f1a96e3
Samantha Wabi <swfullpotential@gmail.com>
2020-06-05 23:09:43 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,40 +1,40 @@
-class CashRegister
-  attr_accessor :total, :discount, :previous_total, :items
+# class CashRegister
+#   attr_accessor :total, :discount, :previous_total, :items
   
-  def initialize(discount = 0)
-    @total = 0 
-    @previous_total
-    @discount = discount
-    @items = []
-  end
+#   def initialize(discount = 0)
+#     @total = 0 
+#     @previous_total
+#     @discount = discount
+#     @items = []
+#   end
   
-   def add_item(title,price,quantity = 1)
-    if quantity>1
-      i=0
-      while i<quantity
-        @items << title
-        i+=1
-      end
-    else
-      @items << title
-    end
-    @total += price*quantity
-    @previous_total = @total
-    @total
-  end
+#   def add_item(title,price,quantity = 1)
+#     if quantity>1
+#       i=0
+#       while i<quantity
+#         @items << title
+#         i+=1
+#       end
+#     else
+#       @items << title
+#     end
+#     @total += price*quantity
+#     @previous_total = @total
+#     @total
+#   end
   
-  def apply_discount()
-    if @discount > 0 
-      @discount = @discount/100.to_f 
-      @total = @total - (@total * (@discount))
-      "After the discount, the total comes to $#{@total.to_i}."
-    else
-      "There is no discount to apply."
-    end
-  end
+#   def apply_discount()
+#     if @discount > 0 
+#       @discount = @discount/100.to_f 
+#       @total = @total - (@total * (@discount))
+#       "After the discount, the total comes to $#{@total.to_i}."
+#     else
+#       "There is no discount to apply."
+#     end
+#   end
   
-  def void_last_transaction
-    @total = @previous_total
-  end
+#   def void_last_transaction
+#     @total = @previous_total
+#   end
 
-end
+# end
