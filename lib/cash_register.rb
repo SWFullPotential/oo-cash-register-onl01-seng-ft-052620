33222a0ddf37fa5b712c7fbca4633fcb9b71cc31
Samantha Wabi <swfullpotential@gmail.com>
2020-06-05 23:08:05 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,8 +1,9 @@
 class CashRegister
-  attr_accessor :total, :discount, :last_transaction_amount, :items
+  attr_accessor :total, :discount, :previous_total, :items
   
   def initialize(discount = 0)
     @total = 0 
+    @previous_total
     @discount = discount
     @items = []
   end
